517b1e972e7efc5fe99c0f08f876ae0484cb9319
Thanaphat-Boonruck <99864890+Thanaphat-Boonrak@users.noreply.github.com>
2025-09-12 02:53:31 +0700

    section 6

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { Nav } from '../layout/nav/nav';
 import { AccountService } from '../core/services/account-service';
 import { HomePage } from '../features/home-page/home-page';
@@ -8,27 +8,11 @@ import { User } from '../type/user';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, HomePage],
+  imports: [Nav, RouterOutlet],
+
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  protected readonly title = signal('frontend');
-  protected member = signal<User[]>([]);
-  private http = inject(HttpClient);
-  private accountService = inject(AccountService);
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-  ngOnInit(): void {
-    this.http.get('https://localhost:8080/api/members').subscribe({
-      next: (res) => console.log(res),
-      error: (err) => console.log(err),
-      complete: () => console.log('Success+'),
-    });
-    this.setCurrentUser();
-  }
+export class App {
+  protected router = inject(Router);
 }
--- frontend/src/layout/nav/nav.ts
+++ frontend/src/layout/nav/nav.ts
@@ -1,31 +1,38 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected creds: any = {};
   protected accountService = inject(AccountService);
-  protected loginStatus = signal(false);
-
+  private router = inject(Router);
+  private toast = inject(ToastService);
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: (res) => {
-        console.log(res);
-        this.loginStatus.set(true);
+        this.creds = {};
+        this.toast.success('Login Success');
+        this.router.navigateByUrl('/members');
       },
-      error: (err) => console.log(err),
+      error: (err) => {
+        console.log(err);
+        this.toast.error(err.error);
+      },
       complete: () => console.log('Sucess'),
     });
   }
 
   logout() {
-    this.loginStatus.set(false);
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
